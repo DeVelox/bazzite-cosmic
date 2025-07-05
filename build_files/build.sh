aa0525b23e6da6ac528ab9a9ea49c99049ed832d
DeVelox <DeVelox@users.noreply.github.com>
2025-07-05 13:46:27 +0200

    Add COSMIC

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,8 +9,9 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 -y copr enable ryanabx/cosmic-epoch
+dnf5 -y install cosmic-desktop
+dnf5 -y copr disable ryanabx/cosmic-epoch
 
 # Use a COPR Example:
 #
@@ -21,4 +22,5 @@ dnf5 install -y tmux
 
 #### Example for enabling a System Unit File
 
-systemctl enable podman.socket
+systemctl disable sddm.service
+systemctl enable cosmic-greeter.service
